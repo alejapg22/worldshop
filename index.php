e8f68cf0edffa87600512d6aa0441346a1561270
deyciflorez <dacyflowers93@gmail.com>
2020-08-28 21:18:43 -0500

    archivo modificado

--- index.php
+++ index.php
@@ -8,10 +8,39 @@
 	<?php ?>
 <p> alejandra</p>
 <body style="background-color: burlywood;">
-	  <center><img  src="ima.ropa.png" alt="img.galeria" width="30%" ></center>
-    <br>
-       <br><form action="chrome-extension://chnacmlpiecdhgkdgeoipkmdbekengck/ntp2.html" method="POST">
-       <label for="nombre">Nombre:</label><br>
+	<center><img  src="ima.ropa.png" alt="img.galeria" width="30%" ></center>
+      <br>
+        <table>
+            <caption></caption>
+            <tr>
+                <th>Refencia</th>
+                <th>Cantidad</th>
+                <th>Producto</th>
+                <th>Precio por unidad</th>
+            </tr>
+             <tr>
+                <td>1586</td>
+                <td>150</td>
+                <td>Overol</td>
+                <td>$80.000</td>
+            </tr>
+             <tr>
+                <td>1587</td>
+                <td>96</td>
+                <td>Blusa</td>
+                <td>$40.000</td>
+            </tr>
+            <tr>
+                <td>1588</td>
+                <td>236</td>
+                <td>Pantalón</td>
+                <td>$50.000</td>
+            </tr>
+        </table>
+        <br>
+        <br>
+        <form action="chrome-extension://chnacmlpiecdhgkdgeoipkmdbekengck/ntp2.html" method="POST">
+        <label for="nombre">Nombre:</label><br>
         <input type="text" name="nombre"><br>
 
         <label for="Apellido">Apellido</label><br>
@@ -26,7 +55,7 @@
         <label for="nombre">dirección:</label><br>
         <input type="text" name="dirección"><br>
 
-       <label for="nombre">telélefono:</label><br>
+        <label for="nombre">telélefono:</label><br>
         <input type="number" name="telélefono"><br>
         
         <label for="email"> Email:</label><br>
@@ -35,8 +64,6 @@
     
     <h1 aria-flowto="d" ><a  ari href="https://www.chevignon.com.co/?gclid=Cj0KCQjws536BRDTARIsANeUZ5-RlUG_u4r-PgzyqCHWDyHor3_vnv7ajc80194urzyCnYX8p8t6YpoaAh2vEALw_wcB" target="_blank"  >  Tienda chevignon </a></h1>
     <br>
-   <center> <iframe width="560" height="315" src="https://www.youtube.com/embed/TzfMa_v6gEI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-
+   <center> <iframe width="560" height="315" src="https://www.youtube.com/embed/7zgtuZXb1wE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
