f877777362b3959f365f4649ab92e691a5b2c0c6
deyciflorez <dacyflowers93@gmail.com>
2020-09-26 02:00:18 -0500

    ensayo

--- index.php
+++ index.php
@@ -21,41 +21,41 @@
 			</ul>
 		</nav><br>
 	</header>
+	<hr>
 	<section class="sec">
-	   <article class="sec" id="d1"><img src="img/hombre.jpg " ></article>
-	    <article class="sec" id="d2"><img src="img/mujer.jpg">   </article>
+	   <a href="cat.-hombre.html"><article class="sec" id="d1"><img src="img/hombre.jpg " ></article></a>
+	    <a href="#"><article class="sec" id="d2"><img src="img/mujer.jpg">   </article></a>
 	    <div class="CallToAction">
 	    	<h1 class="p2">¿Quieres lucir con estilo? </h1>
 	        <img id="w" src="img/w.png">
 	        <h2 id="p1">!Contactanos ahora¡</h2>
 	    </div>
 	</section>
+	<hr>
+	<footer class="footer">
+		<section>
+			<div class="divFooter">
+				<img src="img/logo.png">
+			</div>
+			<div class="divFooter">
+				<ul>
+				   <a href="#">Inicio</a><br>
+				   <a href="#">Nosotros</a><br>
+				   <a href="#">Productos</a><br>
+				   <a href="#">Contactenos</a>
+				</ul>
+			</div>
+			<div class="divFooter">
+				<h4>Contactenos</h4>
+				<a href="https://www.facebook.com/" target="_blank"><img src="img/facebook.png"></a>
+				<a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.png"></a>
+				<a href="https://web.whatsapp.com/" target="_blank"><img src="img/whatsapp.png"></a>
+				<a href="https://twitter.com/?lang=es" target="_blank"><img src="img/twitter.png"></a>
+			</div>
+		</section>
+		<hr>
+		<p>&copy; Copyrigh - Derechos reservados</p>
+	</footer>
 </body>
-
-
-<footer>
-	<section>
-		<div style="float: left;">
-			<img src="img/logo.png">
-		</div>
-		<div style="float: left;">
-			<address>ejemplo@ejemplo.com</address>
-			<ul>
-				<a href="#">Inicio</a><br>
-				<a href="#">Nosotros</a><br>
-				<a href="#">Productos</a><br>
-				<a href="#">Contactenos</a>
-			</ul>
-		</div>
-		<div>
-			<h4>Contactenos</h4>
-			<a href="#"><img src="img/facebook.png"></a>
-			<a href="#"><img src="img/instagram.png"></a>
-			<a href="#"><img src="img/whatsapp.png"></a>
-			<a href="#"><img src="img/twitter.png"></a>
-		</div>
-	</section>
-</footer>
-
 
  </html>
