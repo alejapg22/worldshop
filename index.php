2602e871687bdb47e2bc1117769a173d30895174
deyciflorez <dacyflowers93@gmail.com>
2020-09-26 00:23:10 -0500

    prueba

--- index.php
+++ index.php
@@ -1,81 +1,60 @@
 <!DOCTYPE html>
  <html>
-
-
- <head>
-
-
- <title>worldshop</title>
-
-
- <meta charset="UTF-8">
-
-
- <link rel= "stylesheet" type= "text/css" href= "estilo.css">
-
-
- </head>
-
-
+ 	<head>
+	 	<title>worldshop</title>
+	 	<meta charset="UTF-8">
+	 	<link rel= "stylesheet" type= "text/css" href= "estilo.css">
+	 	<link rel="icon" type="ico" href="img/favicon.png">
+ 	</head>
 <body>
-<?php?>
-<header >
-
-
- </header>
-
-
- <nav>
-
-
- <br>
- 
- <img class="logo" src="img/logo.jpeg"><br>
-
-
-
- <img class="car" src="img/ca.png">
- <img class="bus" src="img/bus.png">
-
-
- <ul id = "menu">
-
-
- <li  href="index.html"> <a> Inicio </a></li>
-
-
- <li href="nosotros.html"> <a> Nosotros </a> </li>
-
-
- <li  href="productos.html"> <a> Productos </a></li>
-
-
- <li  href="contactenos.html"> <a> Contactenos </a></li>
-
-
- </ul>
- 
- </nav>
-
-
-<br>
-
-<section  class="sec" >
-   <article class="sec" id="d1"><img src="img/hombre.jpg " ></article>
-    <article class="sec" id="d2"><img src="img/mujer.jpg">      </article>
-    <div ><h1 class="p2">¿Quieres lucir con estilo? </h1>
-        <img id="w" src="img/w.png">
-        <h2 id="p1">!Contactanos ahora¡</h2>
-    </div
-</section>
-
-
- </body>
+	<?php?>
+	<header >
+ 		<nav><br>
+		 	<img class="logo" src="img/logo.jpeg"><br>
+			<img class="car" src="img/ca.png">
+		 	<img class="bus" src="img/bus.png">
+			<ul id = "menu">
+				<li  href="index.html"> <a> Inicio </a></li>
+				<li href="nosotros.html"> <a> Nosotros </a> </li>
+				<li  href="productos.html"> <a> Productos </a></li>
+				<li  href="contactenos.html"> <a> Contactenos </a></li>
+			</ul>
+		</nav><br>
+	</header>
+	<section class="sec">
+	   <article class="sec" id="d1"><img src="img/hombre.jpg " ></article>
+	    <article class="sec" id="d2"><img src="img/mujer.jpg">   </article>
+	    <div class="CallToAction">
+	    	<h1 class="p2">¿Quieres lucir con estilo? </h1>
+	        <img id="w" src="img/w.png">
+	        <h2 id="p1">!Contactanos ahora¡</h2>
+	    </div>
+	</section>
+</body>
 
 
 <footer>
-
-
+	<section>
+		<div style="float: left;">
+			<img src="img/logo.png">
+		</div>
+		<div style="float: left;">
+			<address>ejemplo@ejemplo.com</address>
+			<ul>
+				<a href="#">Inicio</a><br>
+				<a href="#">Nosotros</a><br>
+				<a href="#">Productos</a><br>
+				<a href="#">Contactenos</a>
+			</ul>
+		</div>
+		<div>
+			<h4>Contactenos</h4>
+			<a href="#"><img src="img/facebook.png"></a>
+			<a href="#"><img src="img/instagram.png"></a>
+			<a href="#"><img src="img/whatsapp.png"></a>
+			<a href="#"><img src="img/twitter.png"></a>
+		</div>
+	</section>
 </footer>
 
 
