e96f1293ce785ca4c961b4016f2841d227810d1d
deyciflorez <dacyflowers93@gmail.com>
2020-10-14 14:58:52 -0500

    bootstrap

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
  	<head>
 	 	<title>worldshop</title>
 	 	<meta charset="UTF-8">
+	 	<link rel="stylesheet" href="css/bootstrap.css">
 	 	<link rel= "stylesheet" type= "text/css" href= "estilo.css">
 	 	<link rel="icon" type="ico" href="img/favicon.png">
  	</head>
 <body>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
 	<?php?>
 	<header >
 		<div class="barra-top">
