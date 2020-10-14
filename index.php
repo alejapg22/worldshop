626d5ef235bc8acda68d22b13df6d17fdbd4fd29
alejapg <aleja4732@gmail.com>
2020-10-14 17:54:52 -0500

    avance

--- index.php
+++ index.php
@@ -6,29 +6,68 @@
 	 	<link rel="stylesheet" href="css/bootstrap.css">
 	 	<link rel= "stylesheet" type= "text/css" href= "estilo.css">
 	 	<link rel="icon" type="ico" href="img/favicon.png">
+	 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
  	</head>
 <body>
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+	
 	<?php?>
 	<header >
-		<div class="barra-top">
+		<section class="barra-top ">
+			<div class="dropdown">
 			<p>info@worldshop.com</p>
-		</div>
- 		<nav><br>
-		 	<a href=""><img class="logo" src="img/logo.jpeg"></a>
-		 	<br>
-			<img class="car" src="img/ca.png">
-		 	<!--<img class="bus" src="img/bus.png">-->
-		 	<input class="buscador" type="text" name="buscador">
-			<ul id = "menu">
-				<li><a href="index.php">Inicio</a></li>
-				<li><a href="#">Nosotros</a> </li>
-				<li><a href="hombre.html">Productos</a></li>
-				<li><a href="#">Contactenos</a></li>
-			</ul>
-		</nav><br>
-	</header>
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Cuenta</a>
+             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+             <a class="dropdown-item" href="#">Iniciar Sesión</a>
+              <a class="dropdown-item" href="#">Crear Cuenta</a>
+             
+             </div>
+             </div>
+             </section>
+
+		<!--menu-->
+ 		<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        
+              <img class="logo" src="img/Logo.png">
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+        </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+         <ul class="navbar-nav">					
+         <li class="nav-item active ">
+        <a style="color:black"; class="nav-link" href="#">Inico <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a style="color:black"; class="nav-link" href="#">Nosotros </a>
+      </li>
+      <li class="nav-item">
+        <a  style="color:black"; class="nav-link" href="#">Productos</a>
+        <li class="nav-item">
+        <a  style="color:black"; class="nav-link" href="#">Contactenos</a>
+      </li>
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="Buscar" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" ><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+  <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+</svg></button>
+      </form>
+
+      <button href="#"  ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+     <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+       </svg></button>
+      
+      <!--<button class="carrito"><span class=" fas fa-shopping-cart"></span>  </button> -->
+
+    
+    
+    </ul>
+  </div>
+</nav>
+ 		</nav>
+
+			
+		 	
 	<section class="sec">
 	  	<article id="d1"><a href="hombre.html"><img src="img/hombre.jpg"></a>
 	  	</article>
@@ -62,9 +101,11 @@
 				<a href="https://twitter.com/?lang=es" target="_blank"><img src="img/twitter.png"></a>
 			</div>
 		</section>
-		<hr>
-		<p>&copy; Copyrigh - Derechos reservados</p>
+		<hr>	<p>&copy; Copyrigh - Derechos reservados</p>
 	</footer>
-</body>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    </body>
 
- </html>
+     
+    </html>
