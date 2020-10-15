a03de7df8a6448d6d341cab52c0150081f3da9de
alejapg <aleja4732@gmail.com>
2020-10-14 21:24:46 -0500

    avanzar

--- index.php
+++ index.php
@@ -47,17 +47,15 @@
       </li>
       <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="Buscar" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" ><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <button class="btn btn-outline-success my-2 my-sm-0 navbar navbar-light  " style="background-color: orangered;" type="submit" ><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor " xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
   <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
 </svg></button>
-      </form>
+         
 
-      <button href="#"  ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <button href="#"b class=""><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
      <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
        </svg></button>
-      
-      <!--<button class="carrito"><span class=" fas fa-shopping-cart"></span>  </button> -->
 
     
     
@@ -76,7 +74,9 @@
 	    <div class="CallToAction">
 	    	<h1 class="p2">¿Quieres lucir con estilo?</h1>
 	        <img id="w" src="img/w.png">
-	        <h2 id="p1">!Contactanos ahora¡</h2>
+            
+	       <button type="button" div="botoncontacto" class="btn btn-danger btn-lg  ">¡Contactanos ahora!</button>
+	        
 	    </div>
 	</section>
 	<hr>
@@ -85,27 +85,32 @@
 			<div class="divFooter">
 				<img src="img/Logo.png">
 			</div>
-			<div class="divFooter">
-				<ul>
-				   <a href="#">Inicio</a><br>
-				   <a href="#">Nosotros</a><br>
-				   <a href="#">Productos</a><br>
-				   <a href="#">Contactenos</a>
-				</ul>
+			<div class="container-fluid">
+				<div class="row">
+			<div class="btn-group col-md-6  ">
+				<button class="btn btn-default btn-lg">Inicio</button>
+				<button class="btn btn-default btn-lg">Nosotros</button>
+				<button class="btn btn-default btn-lg">Productos</button>
+				<button class="btn btn-default btn-lg">Contactenos</button>
+				
 			</div>
-			<div class="divFooter">
-				<h4>Contactenos</h4>
+
+			<div class="divFooter col-md-1">
+				
 				<a href="https://www.facebook.com/" target="_blank"><img src="img/facebook.png"></a>
 				<a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.png"></a>
 				<a href="https://web.whatsapp.com/" target="_blank"><img src="img/whatsapp.png"></a>
 				<a href="https://twitter.com/?lang=es" target="_blank"><img src="img/twitter.png"></a>
 			</div>
 		</section>
-		<hr>	<p>&copy; Copyrigh - Derechos reservados</p>
+
+		<hr >
+		
+			<p>&copy; Copyrigh - Derechos reservados</p>
+
 	</footer>
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     </body>
-
      
     </html>
