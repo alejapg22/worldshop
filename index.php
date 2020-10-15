38a37abdcaaffd59e35f6fd5d9803d1598c7b06d
deyciflorez <dacyflowers93@gmail.com>
2020-10-14 19:08:56 -0500

    nose que poner

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
 	 	<link rel= "stylesheet" type= "text/css" href= "estilo.css">
 	 	<link rel="icon" type="ico" href="img/favicon.png">
  	</head>
-<body>
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+<body>	
 	<?php?>
 	<header >
 		<div class="barra-top">
@@ -65,6 +63,9 @@
 		<hr>
 		<p>&copy; Copyrigh - Derechos reservados</p>
 	</footer>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
 </body>
 
  </html>
