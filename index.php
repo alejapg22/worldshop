8e21838d42074e2c8b0801d16a626080555e0c2b
deyciflorez <dacyflowers93@gmail.com>
2020-10-27 10:02:46 -0500

    modificacion-Final

--- index.php
+++ index.php
@@ -266,7 +266,10 @@
         </div>
         </div>
         </div>
-      </section><br><hr >
+      </section>
+      <br>  
+      <br>
+      <hr>
       <p style="color:white;">&copy; Copyrigh - Derechos reservados</p>
     </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
